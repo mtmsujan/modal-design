5a4be5fcb3a6f7ca1dea0aaa10d410eb0d08c755
mtmsujan <mtmsujon@gmail.com>
2024-08-19 14:44:33 +0600

    Add toggle button dark light mode

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-document.getElementById('open-modal').addEventListener('click', function() {
-    document.getElementById('modal').classList.remove('hidden');
-});
+document.addEventListener('DOMContentLoaded', function () {
+    const modalToggleButton = document.querySelector('[data-modal-toggle="default-modal"]');
+    const modal = document.getElementById('default-modal');
+    const modalCloseButtons = document.querySelectorAll('[data-modal-hide="default-modal"]');
 
-document.getElementById('ok-btn').addEventListener('click', function() {
-    document.getElementById('modal').classList.add('hidden');
+    modalToggleButton.addEventListener('click', function () {
+        modal.classList.remove('hidden');
+    });
+
+    modalCloseButtons.forEach(button => {
+        button.addEventListener('click', function () {
+            modal.classList.add('hidden');
+        });
+    });
+
+    // Close modal when clicking outside of it
+    window.addEventListener('click', function (event) {
+        if (event.target === modal) {
+            modal.classList.add('hidden');
+        }
+    });
+
+    // Close modal when pressing Escape key
+    const toggleButton = document.getElementById('toggle-bg-button');
+    const modalSevenDiv =document.querySelector('.modal-seven');
+
+    toggleButton.addEventListener('click', function () {
+        modalSevenDiv.classList.toggle('toggle-bg');
+    });
 });
